3f738f343449ce3d9ba984a435a48eb26e8050e8
itay oren <itayoren6@gmail.com>
2025-05-28 18:09:05 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	tokenizer1.py

--- tokenizer1.py
+++ tokenizer1.py
@@ -1,149 +1,137 @@
 from typing import List
-from collections import Counter
-import numpy as np
+from collections import defaultdict
 from base_tokenizer import BaseTokenizer
 
 
 class Tokenizer1(BaseTokenizer):
 
-	def __init__(self, vocab_size):
-		super(Tokenizer1, self).__init__()
-		self.merges = {}
-		self.vocab_size = vocab_size
+    def __init__(self, vocab_size):
+        super(Tokenizer1, self).__init__()
+        self.merges = {}
+        self.vocab_size = vocab_size
 
-		# idx -> pair
-		# adds to the 4 special tokens that already exist
-		self.id_to_token.update({idx: bytes([idx - 4]) for idx in range(4, 260)})
-		# token -> idx
-		# adds to the 4 special tokens that already exist
-		self.token_to_id.update({bytes([idx - 4]): idx for idx in range(4, 260)})
-		# pair -> amount of appearances
-		self.scores = {}
-		self.global_count = Counter()
+        # idx -> pair
+        # adds to the 4 special tokens that already exist
+        self.id_to_token.update({idx: bytes([idx]) for idx in range(256)})
+        # token -> idx
+        # adds to the 4 special tokens that already exist
+        self.token_to_id.update({bytes([idx]): idx for idx in range(256)})
+        # pair -> amount of appearances
+        self.scores = {}
+        self.global_count = None
 
-		# for each token holds the idx of sentences that the word is in them
-		self.vocab_to_sen = {}
-		for i in range(260):
-			self.vocab_to_sen[i] = set()
+        # for each pair holds the idx of sentences that it is in
+        self.pair_to_sen = {}
 
-		# holds all the tokens we merged
-		self.pair_in_vocab = set()
+        # holds all the pairs we've merged
+        self.pair_in_vocab = set()
 
-	def train(self, texts: List[str]) -> None:
-		num_merges = self.vocab_size - 260
-		tokens = self.encode_batch(texts)  # List[List[int]]
-		ids = list(tokens)  # copy so we don't destroy the original list
+    def train(self, texts: List[str]) -> None:
+        num_merges = self.vocab_size - 256
+        tokens = self.encode_batch(texts)
+        ids = list(tokens)  # copy so we don't destroy the original list
+        self.global_count = self.get_stats(ids)  # getting initial counts
+        # Initializing which pair appears in which sentences
+        for i, sen in enumerate(ids):
+            for pair in zip(sen, sen[1:]):
+                if pair not in self.pair_to_sen:
+                    self.pair_to_sen[pair] = set()
+                self.pair_to_sen[pair].add(i)
 
-		sen_changed = [False] * len(ids)
-		pair = None
+        for i in range(num_merges):
+            pair = max(self.global_count, key=self.global_count.get)
+            self.scores[pair] = self.global_count[pair]
+            idx = 256 + i
 
-		for i, sentence in enumerate(tokens):
-			for token in sentence:
-				self.vocab_to_sen[token].add(i)
+            token = self.id_to_token[pair[0]] + self.id_to_token[pair[1]]
+            self.id_to_token[idx] = token
+            self.token_to_id[token] = idx
+            self.pair_in_vocab.add((pair[0], pair[1]))
 
-		for i in range(num_merges):
-			if i % 50 == 0:
-				self.global_count = Counter({k: v for k, v in self.global_count.items() if v > 0})
+            print(f"merging {pair} ({self.id_to_token[pair[0]]}, {self.id_to_token[pair[1]]})"
+                  f" into a new token {idx}")
+            ids = self.merge(ids, pair, idx)
+            self.merges[pair] = idx
 
-			idx = 260 + i
-			# Find the best pair
-			pair, count_pair = self.get_highest_count(ids, sen_changed, idx - 1, pair)
-			# Remember that pair score
-			self.scores[pair] = count_pair
+    def get_stats(self, ids):
+        counts = defaultdict(int)
+        for seq in ids:  # for each sequence of token IDs
+            for i in range(len(seq) - 1):
+                pair = (seq[i], seq[i + 1])  # use a tuple, not list
+                counts[pair] = counts.get(pair, 0) + 1
+        return counts
 
-			# Add this pair to the vocab in his original form
-			self.id_to_token[idx] = self.id_to_token[pair[0]] + self.id_to_token[pair[1]]
-			self.merges[pair] = idx
-			self.token_to_id[pair[0] + pair[1]] = idx
-			self.vocab_to_sen[idx] = set()
+    def merge(self, ids, pair, idx):
+        self.global_count[pair] = 0
+        for i in self.pair_to_sen[pair]:
+            new_ids = []
+            j = 0
+            while j < len(ids[i]) - 1:
+                if ids[i][j] == pair[0] and ids[i][j + 1] == pair[1]:
+                    # updating the global count
+                    # updating which pairs are in which sentences
+                    if j != 0:
+                        self.global_count[(ids[i][j - 1], pair[0])] -= 1
+                        self.global_count[(ids[i][j - 1], idx)] += 1
 
-			print(f"merging {pair} ({self.id_to_token[pair[0]]}, {self.id_to_token[pair[1]]})"
-				  f" into a new token {idx} ({self.id_to_token[idx]})")
+                        if (ids[i][j - 1], idx) not in self.pair_to_sen:
+                            self.pair_to_sen[(ids[i][j - 1], idx)] = set()
+                        self.pair_to_sen[(ids[i][j - 1], idx)].add(i)
+                    if j != len(ids[i]) - 2:
+                        self.global_count[(pair[1], ids[i][j + 2])] -= 1
+                        self.global_count[(idx, ids[i][j + 2])] += 1
 
-			# get all the sentences were pair[0] and pair[1] apeared
-			sen_to_merge = self.vocab_to_sen[pair[0]].intersection(self.vocab_to_sen[pair[1]])
-			sen_changed = [False] * len(ids)
+                        if (idx, ids[i][j + 2]) not in self.pair_to_sen:
+                            self.pair_to_sen[(idx, ids[i][j + 2])] = set()
+                        self.pair_to_sen[(idx, ids[i][j + 2])].add(i)
+                    new_ids.append(idx)
+                    j += 2
 
-			for j in sen_to_merge:
-				# Merge the new pair into the text
-				new_sen, changed = self.merge(ids[j], pair, idx, j)
-				sen_changed[j] = changed
-				ids[j] = new_sen
+                else:
+                    new_ids.append(ids[i][j])
+                    j += 1
+            # checking for the last token
+            if j == len(ids[i]) - 1:
+                new_ids.append(ids[i][j])
 
-		for pair in self.scores.keys():
-			self.pair_in_vocab.add(pair)
+            ids[i] = new_ids
+        return ids
 
-	def get_highest_count(self, ids, changed, new_token, pair):
-		if any(changed):
-			self.global_count[pair] = 0
-			for i, sentence in enumerate(ids):
-				if not changed[i]:
-					continue
-
-				for prev, curr in zip(sentence, sentence[1:]):
-					if prev == new_token or curr == new_token:
-						self.global_count[(prev, curr)] += 1
-						if prev == new_token and curr == new_token:
-							continue
-						elif prev == new_token:
-							self.global_count[(pair[1], curr)] -= 1
-						else:
-							self.global_count[(prev, pair[0])] -= 1
-
-			best_pair, freq = self.global_count.most_common(1)[0]
-			return best_pair, freq
-		else:
-			for sentence in ids:
-				self.global_count.update(zip(sentence, sentence[1:]))
-			best_pair, freq = self.global_count.most_common(1)[0]
-			return best_pair, freq
-
-	def merge(self, sentence, pair, idx, sen_idx=-1):
-		changed = False
-		new_sentence = []
-		i = 0
-		while i < len(sentence):
-			# If we find the pair to merge
-			if i < len(sentence) - 1 and sentence[i] == pair[0] and sentence[i + 1] == pair[1]:
-				changed = True
-				if sen_idx != -1:
-					self.vocab_to_sen[idx].add(sen_idx)
-				new_sentence.append(idx)
-				i += 2  # skip the next token as it's part of the merged pair
-			else:
-				new_sentence.append(sentence[i])
-				i += 1
-		return new_sentence, changed
+    def merge_for_encode(self, sen, pair, idx):
+        new_sen = []
+        j = 0
+        while j < len(sen) - 1:
+            if sen[j] == pair[0] and sen[j + 1] == pair[1]:
+                new_sen.append(idx)
+                j += 2
+            else:
+                new_sen.append(sen[j])
+                j += 1
+        # checking for the last token
+        if j == len(sen) - 1:
+            new_sen.append(sen[j])
+        return new_sen
 
 
-	def encode(self, text: str) -> List[int]:
-		tokens = text.encode('utf-8')
-		tokens = list(map(int, tokens))
-		while True:
-			max_pair = None
-			max_score = 0
-			for pair in zip(tokens, tokens[1:]):
-				if pair not in self.pair_in_vocab:
-					continue
-				if self.scores[pair] > max_score:
-					max_score = self.scores[pair]
-					max_pair = pair
-			if max_pair is None:
-				break
-			new_token = self.merges[max_pair]
-			tokens, _ = self.merge(tokens, max_pair, new_token)
-		return tokens
+    def encode(self, text: str) -> List[int]:
+        tokens = text.encode('utf-8')
+        tokens = list(map(int, tokens))
+        while True:
+            max_pair = None
+            max_score = 0
+            for pair in zip(tokens, tokens[1:]):
+                if pair not in self.pair_in_vocab:
+                    continue
+                if self.scores[pair] > max_score:
+                    max_score = self.scores[pair]
+                    max_pair = pair
+            if max_pair is None:
+                break
+            new_token = self.merges[max_pair]
+            tokens = self.merge_for_encode(tokens, max_pair, new_token)
+        return tokens
 
-	def decode(self, token_ids: List[int]) -> str:
-		tokens = b"".join(self.id_to_token[idx] for idx in token_ids)
-		text = tokens.decode("utf-8", errors="replace")
-		return text
-
-	def get_vocab_size(self) -> int:
-		"""
-		Get the size of the vocabulary
-
-		Returns:
-			The number of tokens in the vocabulary
-		"""
-		return self.vocab_size
+    def decode(self, token_ids: List[int]) -> str:
+        tokens = b"".join(self.id_to_token[idx] for idx in token_ids)
+        text = tokens.decode("utf-8", errors="replace")
+        return text
